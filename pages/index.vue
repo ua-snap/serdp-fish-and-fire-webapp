6d6a67aafe26720f2ea821b374f9de80598e11c2
Craig Stephenson <crstephenson@alaska.edu>
2023-01-25 14:48:48 -0900

    Remove extra ~ from areas store import.

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script setup lang="ts">
-import { useAreas } from '~~/stores/areas'
+import { useAreas } from '~/stores/areas'
 const areas = useAreas()
 areas.fetchAreaOptions()
 </script>
