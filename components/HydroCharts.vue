f8ed8fe257b7784ca72c7a9b729630dd18c9d13a
Craig Stephenson <crstephenson@alaska.edu>
2023-04-27 15:22:51 -0800

    Add descriptive Plotly filename for hydrographs.

--- components/HydroCharts.vue
+++ components/HydroCharts.vue
@@ -312,6 +312,9 @@ const renderPlot = () => {
             'autoScale2d',
             'resetScale2d',
           ],
+          toImageButtonOptions: {
+            filename: 'hydrograph',
+          },
         }
       )
     }
