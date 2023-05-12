fa8bf4d3ef631309a5f7fc28e9841ac7e4da28cc
Craig Stephenson <crstephenson@alaska.edu>
2023-05-12 12:31:05 -0800

    More tweaks following merge conflict resolution.

--- components/IntersectingAreas.vue
+++ components/IntersectingAreas.vue
@@ -2,19 +2,23 @@
   <div class="content pr-4">
     <div v-if="store.matchedAreaNames.length == 0">
       <p class="is-size-5 mb-5">Find a place of interest:</p>
-      <ul class="is-size-5">
-        <li>
+      <ul class="list is-size-5">
+        <li class="list-item">
           Click the map within the highlighted region, <em>or&hellip;</em>
         </li>
-        <li>Start typing a name in the box above, <em>or&hellip;</em></li>
-        <li>Click to choose from the 300+ place names in the box above</li>
+        <li class="list-item">
+          Start typing a name in the box above, <em>or&hellip;</em>
+        </li>
+        <li class="list-item">
+          Click to choose from the 300+ place names in the box above
+        </li>
       </ul>
     </div>
     <h5 class="title is-5" v-if="store.matchedAreaNames.length > 0">
       Found {{ store.matchedAreaNames.length }} matching areas for
       {{ store.point.lat }}&deg;N, {{ store.point.lng }}&deg;W
     </h5>
-    <ul class="list">
+    <ul class="list is-size-5">
       <li v-for="areaName in store.matchedAreaNames" class="list-item">
         <a @click="select(areaName)">{{ areaName }}</a>
       </li>
