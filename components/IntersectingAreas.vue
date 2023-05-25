0e3b868cd0fb0fda323ed8eb3fb8ad6612017241
Craig Stephenson <crstephenson@alaska.edu>
2023-05-25 11:33:30 -0800

    Add explanation of the types of places that are selectable.

--- components/IntersectingAreas.vue
+++ components/IntersectingAreas.vue
@@ -13,6 +13,18 @@
           Click to choose from the 300+ place names in the box above
         </li>
       </ul>
+      <p class="is-size-5 mb-5">Types of places include:</p>
+      <ul class="list is-size-5">
+        <li class="list-item">
+          Alaska Department of Fish and Game (ADF&G) Game Management Units
+        </li>
+        <li class="list-item">
+          Alaska Fire Service (AFS) Fire Management Units
+        </li>
+        <li class="list-item">Park and Conservation Units</li>
+        <li class="list-item">Department of Defense (DOD) Lands</li>
+        <li class="list-item">Hydrologic Units Codes (HUCs)</li>
+      </ul>
     </div>
     <h5 class="title is-5" v-if="store.matchedAreaNames.length > 0">
       Found {{ store.matchedAreaNames.length }} matching areas for
